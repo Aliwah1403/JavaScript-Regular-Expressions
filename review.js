eeefc6f0ca4e54017fcf91f1d2758a4f899619b1
Curtis Aliwah <aliwahcurtis5@gmail.com>
2023-05-09 12:37:04 +0300

    Changes to review.js

--- review.js
+++ review.js
@@ -28,3 +28,37 @@ let repeatStr = "Repeat, repeat, REPEAT";
 let repeatRegex = /repeat/gi;
 let repeatResult = repeatStr.match(repeatRegex);
 console.log(repeatResult);
+
+// wildcard (.)
+let humStr = "I'll hum a song for you";
+let hugStr = "Let me give you a hug";
+let hamStr = "I like ham";
+let humRegex = /hu./;
+console.log(humRegex.test(humStr));
+console.log(humRegex.test(hugStr));
+console.log(humRegex.test(hamStr));
+
+//character classes[]
+let batStr = "bat";
+let butStr = "but";
+let bitStr = "bit";
+let btRegex = /b[aui]t/;
+console.log(batStr.match(btRegex));
+console.log(butStr.match(btRegex));
+console.log(bitStr.match(btRegex));
+
+//range in character set[-]
+let meatStr = "meat";
+let beatStr = "beat";
+let seatStr = "seat";
+let veatStr = "veat";
+let eatRegex = /[a-s]eat/;
+console.log(meatStr.match(eatRegex));
+console.log(beatStr.match(eatRegex));
+console.log(seatStr.match(eatRegex));
+console.log(veatStr.match(eatRegex));
+
+//number and letter range
+let mixStr = "James007";
+let mixRegex = /[a-z0-9]/ig;
+console.log(mixStr.match(mixRegex));
